2c6a5e8f313f5aae07a32a236e93fa3da93e6da5
Jesús Fernández Abeledo <suso@users.noreply.github.com>
2025-12-15 14:46:51 +0100

    Define comando axeHighlight para auditoría interactiva

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,11 +1,36 @@
-import 'cypress-axe';
+import "cypress-axe";
+import { annotateViolation, clearHighlights } from "./highlight.js";
 
-Cypress.Commands.add('inicializarAxe', () => {
+Cypress.Commands.add("inicializarAxe", () => {
   cy.injectAxe();
 });
 
-Cypress.Commands.add('auditarAccesibilidad', (context = null, options = null) => {
+Cypress.Commands.add("auditarAccesibilidad", (context = null, options = null) => {
   cy.checkA11y(context, options, (violations) => {
-    cy.task('saveA11yResults', { url: cy.url(), violations });
+    cy.task("saveA11yResults", { url: cy.url(), violations });
+  });
+});
+
+Cypress.Commands.add("axeHighlight", (violations = []) => {
+  return cy.window().then((win) => {
+    clearHighlights(win);
+
+    if (!Array.isArray(violations) || violations.length === 0) {
+      return [];
+    }
+
+    const annotated = [];
+
+    violations.forEach((violation, index) => {
+      const annotation = annotateViolation(win, violation, index + 1);
+      if (annotation) {
+        annotated.push({
+          ...violation,
+          annotation,
+        });
+      }
+    });
+
+    return annotated;
   });
 });
